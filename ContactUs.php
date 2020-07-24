57495cc34ba6ee78e1da13004c58bf2928dca738
zarahussein <43815791+zarahussein@users.noreply.github.com>
2020-07-24 23:02:24 +0400

    updated

---
+++ ContactUs.php
@@ -0,0 +1,31 @@
+<?php 
+$thisPage="campuses"; ?>
+
+<!DOCTYPE html>
+<html>
+<html lang="en">
+<meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+<head>
+    <title>Resitwebsite </title>
+    <link rel="Stylesheet" href="main.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script src="javascript.js"></script>
+
+</head>
+
+
+<body>
+
+    <?php include("phpincludes/header.php"); ?>
+
+
+
+</body>
+
+</html>
